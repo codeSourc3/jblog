09c1b91be5cace26b548e33416bd84f8f9ce405e
Enzo M <47283455+codeSourc3@users.noreply.github.com>
2019-12-02 20:41:25 -0600

    Update BLOG_MODEL.java

--- src/main/java/com/kingschan/blog/common/enums/BLOG_MODEL.java
+++ src/main/java/com/kingschan/blog/common/enums/BLOG_MODEL.java
@@ -1,17 +1,10 @@
 package com.kingschan.blog.common.enums;
 /**
- * 
-*    
-* 类名称：BLOG_MODEL   
-* 类描述：   博客模式，前端和后台
-* 创建人：kings.chan
-* 创建时间：2016-7-25 下午3:17:44   
-* 修改人：   
-* 修改时间：
-* 项目：ROOT
-* 修改备注：   
-* @version    
-*
+ *  
+ * 博客模式，前端和后台
+ * @author kings.chan
+ * @version 12.2.2019       
+ *
  */
 public enum BLOG_MODEL {
 	FONT_MODEL,ADMIN_MODEL
